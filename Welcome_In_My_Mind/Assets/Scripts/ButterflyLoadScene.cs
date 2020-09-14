60a611e4269315f20043ceed65ce88ca03e6a63b
katanaiis <61142950+katanaiis@users.noreply.github.com>
2020-09-14 16:30:02 +0200

    script audio?

--- Welcome_In_My_Mind/Assets/Scripts/ButterflyLoadScene.cs
+++ Welcome_In_My_Mind/Assets/Scripts/ButterflyLoadScene.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 public class ButterflyLoadScene : MonoBehaviour
 {
     public string sceneName;
+    //public GameObject butterfly;
+
+    //private Animator animator;
+    //private AudioSource audioSource;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +21,11 @@ public class ButterflyLoadScene : MonoBehaviour
     // Update is called once per frame
     public void OnMouseDown()
     {
+        //AudioSource sound = butterfly.GetComponent<AudioSource>();
+        //butterfly.GetComponent<Animation>().Play();
+        //sound.Play();
+
+        //wait 3-5 seconde ou la fin de l'anim et du sons
         SceneManager.LoadScene(sceneName);
     }
 }
