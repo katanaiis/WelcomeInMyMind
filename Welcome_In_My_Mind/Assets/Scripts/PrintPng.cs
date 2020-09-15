fb88665099441fdd2da45d88ff39163fb7e25d92
katanaiis <61142950+katanaiis@users.noreply.github.com>
2020-09-15 16:12:49 +0200

    img + script print png

--- Welcome_In_My_Mind/Assets/Scripts/PrintPng.cs
+++ Welcome_In_My_Mind/Assets/Scripts/PrintPng.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PrintPng : MonoBehaviour
 {
+    [SerializeField] private Image customImage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        customImage.enabled = false;
     }
 
     // Update is called once per frame
     void OnMouseDown ()
     {
         Debug.Log("click sur " + this.gameObject.name); //affiche png sur caméra 
+        customImage.enabled = true;
     }
 }
