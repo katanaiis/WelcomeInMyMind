4e2b61f76bd2802a4146072a507c416372210d85
katanaiis <61142950+katanaiis@users.noreply.github.com>
2020-09-25 10:22:39 +0200

    teste script IADog

---
+++ Welcome_In_My_Mind/Assets/Scripts/IADog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IADog : MonoBehaviour
+{
+    public GameObject target;
+
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("U LOSE!");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+}
