701e8e4ecf765968226e29d1905d39651f5347b3
katanais <nanavdh@hotmail.com>
2020-09-28 16:37:40 +0200

    replace sun + script button

---
+++ Welcome_In_My_Mind/Assets/Scripts/ButtonLScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLScript : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        Debug.Log("Prochaine photo Left");
+    }
+}
