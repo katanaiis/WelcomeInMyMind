4fce299f67bbd804f9c0001bfb98d45716251bb6
katanaiis <61142950+katanaiis@users.noreply.github.com>
2020-09-29 12:11:32 +0200

    test butterfly blender + particles system scene vide

---
+++ Welcome_In_My_Mind/Assets/Scripts/ButtonQuit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonQuit : MonoBehaviour
+{
+    public GameObject canvas;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        if (canvas.activeSelf)
+        {
+            canvas.SetActive(false);
+        }
+    }
+}
