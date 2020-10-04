06ddd2b16a1bdaf1ebfb44ae7b0ec511a10cbfa7
katanais <nanavdh@hotmail.com>
2020-10-04 14:06:21 +0200

    script buttonLR test

---
+++ Welcome_In_My_Mind/Assets/Scripts/ButtonLR.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonLR : MonoBehaviour
+{
+    //index = photo courante
+    //ne pas dépasser 0
+    
+    public Image[] img = new Image[10];
+
+    public Image this[img index]
+    {
+        get
+        {
+            i + 1;
+        }
+        set
+        {
+            Debug.Log("Prochaine photo right");
+            return img[index];
+        }
+    }
+
+
+/*    void Left()
+    {
+        Debug.Log("Photo d'avant left");
+        i - 1;
+        if (img i = 0; i < img.Lenght; i++) {
+            return img[index];
+        }
+    }
+
+    void Right()
+    {
+        Debug.Log("Prochaine photo right");
+        i + 1;
+        return img[indexer];
+        if (i == 0)
+        {
+
+        }
+    }*/
+}
