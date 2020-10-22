00b5f46f857b7aa52227237f02e1eb31c49e3706
katanaiis <61142950+katanaiis@users.noreply.github.com>
2020-10-22 16:32:05 +0200

    controller

---
+++ Welcome_In_My_Mind/Assets/Scripts/Play.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Play : MonoBehaviour
+{
+
+    public UnityEvent onClick;
+    //public bool start;
+    //public UnityEvent whenAnimation;
+    public void OnMouseDown()
+    {
+        //start = true;
+        onClick?.Invoke();
+    }
+
+
+    /*public void PlayWithAnimation()
+    {
+        whenAnimation.Invoke();
+    }*/
+}
---
+++ Welcome_In_My_Mind/Assets/Scripts/PlayAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Animator))]
+public class PlayAnim : MonoBehaviour
+{
+    private Animator anim;
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        anim.SetBool("start", true);
+    }
+}
