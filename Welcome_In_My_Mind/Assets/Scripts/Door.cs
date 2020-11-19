b42994021ad32f11c88e9a962472a039d3ce4288
katanais <nanavdh@hotmail.com>
2020-11-19 16:54:59 +0100

    porte + post process

--- Welcome_In_My_Mind/Assets/Scripts/Door.cs
+++ Welcome_In_My_Mind/Assets/Scripts/Door.cs
@@ -10,112 +10,39 @@ public class Door : MonoBehaviour
     quand le player est sorti du collider
     alors tu play anim de fin (ou open en reverse)*/
 
-    /*public Animation doorAnim;
-    public float speed;
-    public float MaxOpenValue;
-    public Transform door;
-    public bool opening = false;
-    public bool closing = false;
-    private float currentvalue = 0;
+
+    //public Transform door;
+    public Animator porte;
+
+
 
     // Update is called once per frame
     void Update()
-    {
-        if (opening) OpenDoor();
-        if (closing) CloseDoor();
-    }
-
-
-    void OnTriggerEnter(Collider obj)
-    {
-        if (obj.transform.name == "Player")
-        {
-            opening = true;
-            closing = false;
-        }
-
-    }
-    private void OnTriggerExit(Collider obj)
-    {
-        if (obj.transform.name == "Player")
-        {
-            opening = false;
-            closing = true;
-        }
-    }
-
-    void OpenDoor()
-    {
-        float movement = speed * Time.deltaTime;
-        currentvalue += movement;
-        if (currentvalue <= MaxOpenValue)
-        {
-            door.position = new Vector3(
-
-                door.position.x,
-                door.position.y,
-                door.position.z + movement
-                );
-        }
-        else
-        {
-            opening = false;
-        }
-    }
-
-    void CloseDoor()
-    {
-        float movement = speed * Time.deltaTime;
-        currentvalue -= movement;
-        if (currentvalue >= 0x0)
-        {
-            door.position = new Vector3(
-
-                door.position.x,
-                door.position.y,
-                door.position.z - movement
-                );
-        }
-        else
-        {
-            closing = false;
-        }
-    }*/
-
-    /*void OnTriggerEnter(Collision other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            Porte.animation.Play("porte");
-
-        }
-    }*/
-
-
-    /*// Start is called before the first frame update
-    void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnTriggerEnter(Collider other)
     {
-        if (IsOnTrigger) // ou othercomparetag player
+        if (other.gameObject.tag == "Player")
         {
-            //play anim Open
-            //play son open door
-            
-        }
-        *//*while () //tant que le player n'est pas sorti du collider
-        {
-            //attendre
-        }*//*
-        else
-        {
-            //alors tu play anim de fin (ou open en reverse)
-            //play son open door
+
+            porte.SetTrigger("open");
+
+ 
+
         }
 
-    }*/
+    }
+    private void OnTriggerExit(Collider other)
+    {
+     
+        if (other.gameObject.tag == "Player")
+        {
+            porte.SetTrigger("close");
+        }
+    }
+
+
 }
